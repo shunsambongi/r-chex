3a8d18eeca27562e492f7f1610f1f3afad4c6ef1
Shun Sambongi <shundev3@gmail.com>
2020-07-21 10:12:50 -0600

    Add logging tests

--- R/logging.R
+++ R/logging.R
@@ -1,9 +1,12 @@
 log_result <- function(result) {
+  if (!requireNamespace("logger", quietly = TRUE)) {
+    return(invisible(result))
+  }
   pass_level <- getOption("CHEX_PASS_LOG_LEVEL", logger::SUCCESS)
   fail_level <- getOption("CHEX_FAIL_LOG_LEVEL", logger::WARN)
 
   description <- description(result)
-  for (i in 1:vec_seq_along(result)) {
+  for (i in vec_seq_along(result)) {
     res <- result[i]
     desc <- description[i]
     level <- if (res == PASS) {
